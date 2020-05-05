054e3fb54c594f16375898c78effb63950ac084b
Liubov <regnum04.08.83@gmail.com>
2020-05-05 14:18:25 +0300

    3 lesson

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -9,65 +19,66 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true,
  };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько обойдется?");
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце"),
+            b = prompt("Во сколько обойдется?");
 
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
-     a != "" && b != "" && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("Ошибка!");
-        i--;
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != "" && b != "" && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            console.log("Ошибка!");
+            i--;
+        }
     }
 }
 
-/* let i = 0;
-while (i < 2) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько обойдется?");
+chooseExpenses();
 
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
-        a != "" && b != "" && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("Ошибка!");
-        i--;
+function chooseOptExpenses() {
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        
     }
-    i++;
-} */
-
-/* let i = 0;
-do {
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько обойдется?");
-
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
-        a != "" && b != "" && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("Ошибка!");
-        i--;
-    }
-    i++;
-} while (i < 2); */
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уроверь достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уроверь достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уроверь достатка");
-} else {
-    console.log("Произошла ошибка");
 }
+
+chooseOptExpenses();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay + " руб.");
+}
+
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальный уроверь достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уроверь достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уроверь достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings === true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц в Вашего депозита: " + appData.monthIncome + " руб.");
+    }
+}
+
+checkSavings();
